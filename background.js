4bd57e0468ab2b8cf06a6c78338d5c67095eadad
Shargil <yamshargil@gmail.com>
2021-03-24 18:56:59 +0200

    background execute filter script every time when youtube requesting new videos

--- background.js
+++ background.js
@@ -1,26 +1,3 @@
-// On installing the extension
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('On installing!');
-});
-
-// On chancing to the tab
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, (current_tab_info) => {
-        if (current_tab_info.url == "https://www.youtube.com/") {
-            insertFilesToForeground();
-        }
-    });
-});
-
-// On refreshing the tab
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    chrome.tabs.get(tabId, (current_tab_info) => {
-        if (current_tab_info.url == "https://www.youtube.com/") {
-            insertFilesToForeground();
-        }
-    });
-});
-
 // On YouTube request new videos, ask foreground to rerun filter
 const youTubeMoreVideosUrls = [
     "https://www.youtube.com/",
@@ -28,16 +5,14 @@ const youTubeMoreVideosUrls = [
 ]
 
 // onComplete?
-chrome.webRequest.onResponseStarted.addListener(details => {
-    console.log("On YouTube request new videos, rerun foreground")
-    rerunForegroundFilter();
-},
+chrome.webRequest.onResponseStarted.addListener(
+    details => {
+        console.log("On YouTube request new videos, rerun foreground");
+        chrome.scripting.executeScript(
+            {
+                target: { tabId: details.tabId },
+                files: ['fillter.js']
+            },
+            () => { });
+    },
     { urls: youTubeMoreVideosUrls });
-
-
-function insertFilesToForeground() {
-    chrome.tabs.insertCSS(null, { file: './mystyle.css' });
-    chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
-        console.log("i injected from bg to foreground.js")
-    );
-}
