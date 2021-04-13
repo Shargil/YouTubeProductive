475a65d6977a480910f5465291db2713b6aa234f
Shargil <yamshargil@gmail.com>
2021-04-13 23:27:37 +0300

    SearchOnly - working with diffrent events failed

--- contentScripts/searchOnly.js
+++ contentScripts/searchOnly.js
@@ -1,92 +1,67 @@
 console.log("--- searchOnly.js --- ");
+// searchOnly();
 
-// window.addEventListener('load', function () {
+// var count = 0
+// var myInterval = setInterval(() => {
+//     console.log("interval " + myInterval.toString())
 //     searchOnly();
-// });
 
-// function searchOnly() {
-//     alert("--- searchOnly.js --- ");
-//     var endScreenContent = document.getElementsByClassName("ytp-endscreen-content");
-//     if (endScreenContent && endScreenContent[0])
-//         endScreenContent[0].remove();
-
-//     var contents = document.getElementById("contents");
-//     if (contents) {
-//         console.log(contents);
-//         contents.remove();
-//     }
-
-//     var suggestionsCol = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer')[0]
-//     if (suggestionsCol)
-//         suggestionsCol.remove();
-
-//     var subscriptions = document.getElementsByTagName('ytd-guide-section-renderer')[1];
-//     if (subscriptions && subscriptions.querySelector('yt-formatted-string').innerText === "SUBSCRIPTIONS")
-//         subscriptions.remove();
-// }
-
-// let contents = document.getElementById("contents");
-// if (contents) {
-//     console.log(contents);
-//     contents.remove();
-// }
-// let count = 0
-// const myInterval = setInterval(() => {
-//     contents = document.getElementById("contents");
-//     if (contents) {
-//         console.log(contents);
-//         contents.remove();
-//     }
 //     count++;
-//     if (count >= 20) {
+//     if (count >= 10) {
 //         clearInterval(myInterval);
 //     }
-
 // }, 500);
 
+window.addEventListener('load', function () {
+    alert("--- searchOnly.js window load --- ");
+    searchOnly();
+});
+document.addEventListener('load', function () {
+    alert("--- searchOnly.js document load --- ");
+    searchOnly();
+});
+document.addEventListener('reallyAllDone', function () {
+    alert("--- searchOnly.js document reallyAllDone--- ");
+    searchOnly();
+});
+window.addEventListener('reallyAllDone', function () {
+    alert("--- searchOnly.js window reallyAllDone--- ");
+    searchOnly();
+});
 
-onNodeExist('contents', document.getElementsByTagName('body')[0], false,
-    (elementNode) => {
-        if (elementNode) {
-            console.log("delete element: ");
-            console.log(elementNode);
-            elementNode.remove();
-        }
+document.addEventListener("DOMContentLoaded", function () {
+    alert("--- searchOnly.js DOMContentLoaded--- ");
+    searchOnly();
+});
+document.addEventListener('readystatechange', function () {
+    alert("document ready state: " + document.readyState);
+    searchOnly();
+});
+document.onreadystatechange(this, () => {
+    alert("document ready state: " + document.readyState);
+    searchOnly();
+});
+
+
+function searchOnly() {
+    var endScreenContent = document.getElementsByClassName("ytp-endscreen-content");
+    if (endScreenContent && endScreenContent[0])
+        endScreenContent[0].remove();
+
+    var contents = document.getElementById("contents");
+    if (contents) {
+        console.log(contents);
+        contents.remove();
     }
-);
 
-function onNodeExist(id, parent, recursive, callBack) {
-    // If it's already exist
-    var elementNode = document.getElementById(id);
-    if (elementNode) {
-        console.log("onNodeExist call found element right away")
-        callBack(elementNode);
-    }
-    let observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
+    var suggestionsCol = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer')[0]
+    if (suggestionsCol)
+        suggestionsCol.remove();
 
-            for (let i = 0; i < mutation.addedNodes.length; i++) {
-                // do things to your newly added nodes here
-                let node = mutation.addedNodes[i];
-                if (node.id === id) {
-                    console.log("on MutationObserver found element")
-                    callBack(elementNode);
-                }
-            }
-        })
-    });
-
-
-    // let observer = new MutationObserver((mutations) => {
-    //     var elementNode = document.getElementById(id);
-    //     if (elementNode) {
-    //         callBack(elementNode);
-    //         // this.disconnect();
-    //     }
-    // });
-
-    observer.observe(parent || document, {
-        subtree: !!recursive || !parent,
-        childList: true,
-    });
+    var subscriptions = document.getElementsByTagName('ytd-guide-section-renderer')[1];
+    if (subscriptions && subscriptions.querySelector('yt-formatted-string').innerText === "SUBSCRIPTIONS")
+        subscriptions.remove();
 }
+
+
+
