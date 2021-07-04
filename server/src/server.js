06acf5dcbd15f32cfef86c88892abb0cba68d1cc
Shargil <yamshargil@gmail.com>
2021-07-04 17:01:13 +0300

    Sever - connect to local Prod DB

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,10 @@ require('dotenv').config()
 
 // Connect DB
 const mongoose = require('mongoose')
-const uri = "mongodb+srv://admin:qVc450BAG6K0hC7C@youtubeproductive.f2djv.mongodb.net/YouTubeProductive?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://admin:qVc450BAG6K0hC7C@youtubeproductive.f2djv.mongodb.net/YouTubeProductive?retryWrites=true&w=majority";
+const prodUri = "mongodb://localhost:27017/local";
+// If MONGO_DB_URI_DEV is defined connect to -> mongo atlas cloud Dev DB, if not connect to local (that what happens in prod)
+const uri = process.env.MONGO_DB_URI_DEV || prodUri;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
